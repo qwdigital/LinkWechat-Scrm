4cb7f6db27f9082f157667e5a372ad08b3412a90
18356073052 <274131322@qq.com>
2020-12-30 00:08:41 +0800

    群发消息任务代码重构

--- linkwe-wecom/src/main/java/com/linkwechat/wecom/service/impl/WeCustomerMessageOriginalServiceImpl.java
+++ linkwe-wecom/src/main/java/com/linkwechat/wecom/service/impl/WeCustomerMessageOriginalServiceImpl.java
@@ -1,14 +1,26 @@
 package com.linkwechat.wecom.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.linkwechat.common.constant.WeConstans;
+import com.linkwechat.common.utils.StringUtils;
+import com.linkwechat.wecom.client.WeCustomerMessagePushClient;
 import com.linkwechat.wecom.domain.WeCustomerMessageOriginal;
+import com.linkwechat.wecom.domain.dto.message.DetailMessageStatusResultDto;
+import com.linkwechat.wecom.domain.dto.message.QueryCustomerMessageStatusResultDataObjectDto;
+import com.linkwechat.wecom.domain.dto.message.QueryCustomerMessageStatusResultDto;
 import com.linkwechat.wecom.domain.vo.CustomerMessagePushVo;
 import com.linkwechat.wecom.mapper.WeCustomerMessageOriginalMapper;
+import com.linkwechat.wecom.mapper.WeCustomerMessgaeResultMapper;
 import com.linkwechat.wecom.service.IWeCustomerMessageOriginalService;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * 群发消息 原始数据信息表 we_customer_messageOriginal
@@ -22,6 +34,15 @@ public class WeCustomerMessageOriginalServiceImpl extends ServiceImpl<WeCustomer
     @Autowired
     private WeCustomerMessageOriginalMapper weCustomerMessageOriginalMapper;
 
+    @Autowired
+    private WeCustomerMessagePushClient weCustomerMessagePushClient;
+
+    @Autowired
+    private WeCustomerMessgaeResultMapper weCustomerMessgaeResultMapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Override
     public int saveWeCustomerMessageOriginal(WeCustomerMessageOriginal weCustomerMessageOriginal) {
         return weCustomerMessageOriginalMapper.insert(weCustomerMessageOriginal);
@@ -32,5 +53,53 @@ public class WeCustomerMessageOriginalServiceImpl extends ServiceImpl<WeCustomer
         return weCustomerMessageOriginalMapper.selectCustomerMessagePushs(sender, content, pushType, beginTime, endTime);
     }
 
+    @Override
+    public CustomerMessagePushVo CustomerMessagePushDetail(Long messageId)  {
+
+
+        CustomerMessagePushVo customerMessagePushDetail = weCustomerMessageOriginalMapper.findCustomerMessagePushDetail(messageId);
+
+        //拉取消息发送结果
+       // CompletableFuture.runAsync(()->{
+
+            String msgid = customerMessagePushDetail.getMsgid();
+
+            if(StringUtils.isNotEmpty(msgid)){
+
+                List<String> msgIds = null;
+                try {
+                    msgIds = objectMapper.readValue(msgid,new TypeReference<List<String>>() { });
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+
+                if(CollectionUtils.isNotEmpty(msgIds)){
+                    msgIds.forEach(m->{
+
+                        QueryCustomerMessageStatusResultDataObjectDto dataObjectDto=new QueryCustomerMessageStatusResultDataObjectDto();
+
+                        dataObjectDto.setMsgid(m);
+
+                        //拉取发送结果
+                        QueryCustomerMessageStatusResultDto queryCustomerMessageStatusResultDto = weCustomerMessagePushClient.queryCustomerMessageStatus(dataObjectDto);
+                        if (WeConstans.WE_SUCCESS_CODE.equals(queryCustomerMessageStatusResultDto.getErrcode())) {
+
+
+                            List<DetailMessageStatusResultDto> detailList = queryCustomerMessageStatusResultDto.getDetail_list();
+
+                            detailList.forEach(d-> weCustomerMessgaeResultMapper.updateWeCustomerMessgaeResult(messageId,d.getChat_id(),d.getExternal_userid(),d.getStatus(),d.getSend_time()));
+
+                        }
+
+                    });
+                }
+
+            }
+
+      //  });
+
+
+        return weCustomerMessageOriginalMapper.findCustomerMessagePushDetail(messageId);
+    }
 
 }
