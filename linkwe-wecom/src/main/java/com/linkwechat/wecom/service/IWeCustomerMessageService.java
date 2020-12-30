9cc253cffcd54eeb6e8a9ba23d407bdab1c13a07
18356073052 <274131322@qq.com>
2020-12-30 16:17:13 +0800

    重构群发消息任务

--- linkwe-wecom/src/main/java/com/linkwechat/wecom/service/IWeCustomerMessageService.java
+++ linkwe-wecom/src/main/java/com/linkwechat/wecom/service/IWeCustomerMessageService.java
@@ -2,6 +2,7 @@ package com.linkwechat.wecom.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.linkwechat.wecom.domain.WeCustomerMessage;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 群发消息  微信消息表service接口
@@ -12,10 +13,18 @@ import com.linkwechat.wecom.domain.WeCustomerMessage;
 public interface IWeCustomerMessageService extends IService<WeCustomerMessage> {
 
     /**
-     *
      * @param customerMessage
      * @return
      */
     int updateWeCustomerMessageMsgId(WeCustomerMessage customerMessage);
+
+    /**
+     *
+     * @param messageId 微信消息表主键id
+     * @param actualSend 实际发送消息数（客户对应多少人 客户群对应多个群）
+     * @return int
+     */
+    int updateWeCustomerMessageActualSend( Long messageId, Integer actualSend);
+
 
 }
