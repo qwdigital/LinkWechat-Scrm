cbb1c674b1e37aa4e86eef316c9adcbdff6fce06
18356073052 <274131322@qq.com>
2020-12-30 16:53:44 +0800

    查询微信消息发送情况

--- linkwe-wecom/src/main/java/com/linkwechat/wecom/service/impl/WeCustomerMessgaeResultServiceImpl.java
+++ linkwe-wecom/src/main/java/com/linkwechat/wecom/service/impl/WeCustomerMessgaeResultServiceImpl.java
@@ -2,9 +2,13 @@ package com.linkwechat.wecom.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.linkwechat.wecom.domain.WeCustomerMessgaeResult;
+import com.linkwechat.wecom.domain.vo.WeCustomerMessageResultVo;
 import com.linkwechat.wecom.mapper.WeCustomerMessgaeResultMapper;
 import com.linkwechat.wecom.service.IWeCustomerMessgaeResultService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 群发消息  微信消息发送结果表 we_customer_messgaeResult
@@ -14,4 +18,13 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class WeCustomerMessgaeResultServiceImpl extends ServiceImpl<WeCustomerMessgaeResultMapper, WeCustomerMessgaeResult> implements IWeCustomerMessgaeResultService {
+
+    @Autowired
+    private WeCustomerMessgaeResultMapper customerMessgaeResultMapper;
+
+    @Override
+    public List<WeCustomerMessageResultVo> customerMessagePushs(Long messageId, String status) {
+        return customerMessgaeResultMapper.customerMessagePushs(messageId, status);
+    }
+
 }
