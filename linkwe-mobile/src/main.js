870c9c1c26ba2898a44b0bee496701f2a0fb440c
xinlaa <1570728529@qq.com>
2021-01-06 01:53:20 +0800

    js sdk 引入与相关配置

--- linkwe-mobile/src/main.js
+++ linkwe-mobile/src/main.js
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import wx from '@/utils/jweixin-1.2.0.js'
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+window.wx = wx
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app')
