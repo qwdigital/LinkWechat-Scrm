4b240b9e966330852a964b4a94a3339cfdb2738a
xinlaa <1570728529@qq.com>
2021-01-06 18:50:10 +0800

    移动端增加初始配置

--- linkwe-mobile/src/main.js
+++ linkwe-mobile/src/main.js
@@ -1,5 +1,11 @@
 import wx from '@/utils/jweixin-1.2.0.js'
 import Vue from 'vue'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
