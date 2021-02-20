4a2eff9539d4c213ed0f6016eb87c7cb7e0ed024
浪涛飞 <274131322@qq.com>
2021-02-20 21:57:34 +0800

    新客自动拉群

--- linkwe-wecom/src/main/java/com/linkwechat/wecom/mapper/WeCommunityNewGroupMapper.java
+++ linkwe-wecom/src/main/java/com/linkwechat/wecom/mapper/WeCommunityNewGroupMapper.java
@@ -2,6 +2,10 @@ package com.linkwechat.wecom.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.linkwechat.wecom.domain.WeCommunityNewGroup;
+import com.linkwechat.wecom.domain.vo.WeCommunityNewGroupVo;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 社群运营 新客自动拉群 mapper接口
@@ -10,5 +14,28 @@ import com.linkwechat.wecom.domain.WeCommunityNewGroup;
  * @date 2021-02-19
  */
 public interface WeCommunityNewGroupMapper extends BaseMapper<WeCommunityNewGroup> {
+
+    /**
+     * 查询新客自动拉群列表
+     *
+     * @param communityNewGroup 搜索信息
+     * @return {@link WeCommunityNewGroup}s
+     */
+    List<WeCommunityNewGroupVo> selectWeCommunityNewGroupList(WeCommunityNewGroup communityNewGroup);
+
+    /**
+     * 获取新客自动拉群详细信息
+     * @param newGroupId 主键id
+     * @return {@link WeCommunityNewGroupVo} 自动拉群信息
+     */
+    WeCommunityNewGroupVo selectWeCommunityNewGroupById(@Param("newGroupId") Long newGroupId);
+
+    /**
+     * 删除新客自动拉群
+     *
+     * @param idList id列表
+     * @return
+     */
+    int batchRemoveWeCommunityNewGroupIds(List<String> idList);
 
 }
