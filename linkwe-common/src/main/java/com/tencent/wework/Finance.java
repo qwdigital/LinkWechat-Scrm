a840a941e270896b90ddf6886af0faae7676f910
danmo <775144922@qq.com>
2021-03-05 00:06:30 +0800

    fix

--- linkwe-common/src/main/java/com/tencent/wework/Finance.java
+++ linkwe-common/src/main/java/com/tencent/wework/Finance.java
@@ -118,7 +118,7 @@ public class Finance {
             System.loadLibrary("libcurl");
             System.loadLibrary("WeWorkFinanceSdk");
         } else {
-            System.load("/var/jenkins_home/app/projects/libWeWorkFinanceSdk_Java.so");
+            System.load("/data/jenkins_home/app/projects/libWeWorkFinanceSdk_Java.so");
         }
 
     }
