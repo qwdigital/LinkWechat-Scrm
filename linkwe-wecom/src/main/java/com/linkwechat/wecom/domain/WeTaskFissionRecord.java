c4590fbccd2722fc45571221d11ea0fbca235c73
leejoker <1056650571@qq.com>
2021-03-17 23:19:11 +0800

    字段补充

--- linkwe-wecom/src/main/java/com/linkwechat/wecom/domain/WeTaskFissionRecord.java
+++ linkwe-wecom/src/main/java/com/linkwechat/wecom/domain/WeTaskFissionRecord.java
@@ -11,6 +11,8 @@ import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+import java.util.Date;
+
 /**
  * 裂变任务记录对象 we_task_fission_record
  *
@@ -60,6 +62,9 @@ public class WeTaskFissionRecord extends BaseEntity {
     @ApiModelProperty("生成二维码配置id")
     private String configId;
 
+    @ApiModelProperty("完成时间")
+    private Date completeTime;
+
     @Override
     public String toString() {
         return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
@@ -69,6 +74,7 @@ public class WeTaskFissionRecord extends BaseEntity {
                 .append("customerName", getCustomerName())
                 .append("fissNum", getFissNum())
                 .append("configId", getConfigId())
+                .append("completeTime", getCompleteTime())
                 .toString();
     }
 }
