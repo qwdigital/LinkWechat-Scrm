55e4720ce635c00b61affa1a5b352d48d287d78e
danmo <775144922@qq.com>
2021-07-20 00:03:49 +0800

    修复群活码生成二维码失败问题

--- linkwe-common/src/main/java/com/linkwechat/common/utils/file/FileUtil.java
+++ linkwe-common/src/main/java/com/linkwechat/common/utils/file/FileUtil.java
@@ -6,8 +6,6 @@ import com.linkwechat.common.config.RuoYiConfig;
 import com.linkwechat.common.utils.OsUtils;
 import com.linkwechat.common.utils.spring.SpringUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
