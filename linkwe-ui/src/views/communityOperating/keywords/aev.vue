8e9be2a9e5328f50ffacac6fccc067c3352aa454
xinla <1570728529@qq.com>
2021-11-24 01:30:34 +0800

    代码合并问题

--- linkwe-ui/src/views/communityOperating/keywords/aev.vue
+++ linkwe-ui/src/views/communityOperating/keywords/aev.vue
@@ -33,7 +33,11 @@
       </el-form-item>
 
       <el-form-item label="选择群活码">
-        <el-image v-if="groupQrCode && groupQrCode.codeUrl" :src="groupQrCode.codeUrl" class="code-image">
+        <el-image
+          v-if="groupQrCode && groupQrCode.codeUrl"
+          :src="groupQrCode.codeUrl"
+          class="code-image"
+        >
         </el-image>
 
         <el-button
@@ -59,10 +63,9 @@
 
       <PhoneDialog
         :message="form.welcomeMsg || '请输入加群引导语'"
-        :isOther="(groupQrCode && groupQrCode.codeUrl) ? true : false"
+        :isOther="groupQrCode && groupQrCode.codeUrl ? true : false"
       >
-        <el-image style="border-radius: 6px; width: 100px;" :src="groupQrCode.codeUrl">
-        </el-image>
+        <el-image style="border-radius: 6px; width: 100px;" :src="groupQrCode.codeUrl"> </el-image>
       </PhoneDialog>
     </div>
 
@@ -92,24 +95,16 @@ export default {
         taskName: '', // 任务名称
         groupCodeId: '', // 群活码ID
         welcomeMsg: '', // 欢迎语
-        keywords: '', // 关键词
+        keywords: '' // 关键词
       },
       codes: [],
       groupQrCode: {},
       rules: Object.freeze({
-        taskName: [
-          { required: true, message: '该项为必填项', trigger: 'blur' }
-        ],
-        welcomeMsg: [
-          { required: true, message: '该项为必填项', trigger: 'blur' }
-        ],
-        keywords: [
-          { required: true, message: '该项为必填项', trigger: 'blur' }
-        ],
-        groupCodeId: [
-          { required: true, message: '该项为必填项', trigger: 'change' }
-        ],
-      }),
+        taskName: [{ required: true, message: '该项为必填项', trigger: 'blur' }],
+        welcomeMsg: [{ required: true, message: '该项为必填项', trigger: 'blur' }],
+        keywords: [{ required: true, message: '该项为必填项', trigger: 'blur' }],
+        groupCodeId: [{ required: true, message: '该项为必填项', trigger: 'change' }]
+      })
     }
   },
   created() {
@@ -120,31 +115,30 @@ export default {
     /** 获取详情 */
     getDetail(id) {
       this.loading = true
-      getDetail(id)
-        .then(({ data }) => {
-          this.form.taskName = data.taskName
-          this.form.welcomeMsg = data.welcomeMsg
+      getDetail(id).then(({ data }) => {
+        this.form.taskName = data.taskName
+        this.form.welcomeMsg = data.welcomeMsg
 
-          // const keywordList = data.keywordList || []
-          // const keywords = keywordList.map((k) => k.keyword)
-          // this.form.keywords = keywords.join(',')
-          this.form.keywords = data.keywords
+        // const keywordList = data.keywordList || []
+        // const keywords = keywordList.map((k) => k.keyword)
+        // this.form.keywords = keywords.join(',')
+        this.form.keywords = data.keywords
 
-          if (data.groupCodeInfo && data.groupCodeInfo.id) {
-            this.form.groupCodeId = data.groupCodeInfo.id
-            this.codes = [ data.groupCodeInfo ]
-            this.groupQrCode = data.groupCodeInfo
-          } else {
-            this.form.groupCodeId = ''
-            this.codes = []
-            this.groupQrCode = {}
-          }
+        if (data.groupCodeInfo && data.groupCodeInfo.id) {
+          this.form.groupCodeId = data.groupCodeInfo.id
+          this.codes = [data.groupCodeInfo]
+          this.groupQrCode = data.groupCodeInfo
+        } else {
+          this.form.groupCodeId = ''
+          this.codes = []
+          this.groupQrCode = {}
+        }
 
-          this.loading = false
-        })
+        this.loading = false
+      })
     },
     submit() {
-      this.$refs.form.validate((valid) => {
+      this.$refs.form.validate(valid => {
         if (valid) {
           this.loading = true
           if (this.taskId) {
@@ -176,86 +170,10 @@ export default {
       this.groupQrCode = data
       this.form.groupCodeId = data.id
       this.$refs.form.validateField('groupCodeId')
-    },
+    }
   }
 }
 </script>
-
-<template>
-  <div class="wrap" v-loading="loading">
-    <el-form
-      :model="form"
-      ref="form"
-      :rules="rules"
-      label-width="100px"
-    >
-      <el-form-item label="活码名称" prop="taskName">
-        <el-input
-          v-model="form.taskName"
-          maxlength="30"
-          show-word-limit
-          placeholder="请输入名称"
-          clearable
-        ></el-input>
-      </el-form-item>
-      <el-form-item label="关键词" prop="keywords">
-        <el-input
-          v-model="form.keywords"
-          placeholder="请输入关键词,多个词用英文逗号分隔,每个词不超过10个字"
-          clearable
-        ></el-input>
-      </el-form-item>
-      <el-form-item label="加群引导语" prop="welcomeMsg">
-        <el-input
-          type="textarea"
-          v-model="form.welcomeMsg"
-          maxlength="220"
-          show-word-limit
-          :autosize="{ minRows: 5, maxRows: 20 }"
-          placeholder="请输入加群引导语"
-          clearable
-        ></el-input>
-      </el-form-item>
-      <el-form-item label="选择群活码" prop="groupCodeId">
-        <el-image
-          v-if="groupQrCode && groupQrCode.codeUrl"
-          :src="groupQrCode.codeUrl"
-          class="code-image mr10"
-        ></el-image>
-        <el-button
-          type="primary"
-          plain
-          icon="el-icon-plus"
-          size="mini"
-          @click="dialogVisibleSelectQrCode = true"
-          >{{ groupQrCode && groupQrCode.codeUrl ? '修改' : '添加' }}</el-button
-        >
-      </el-form-item>
-      <el-form-item label=" ">
-        <el-button type="primary" @click="submit">保存</el-button>
-        <el-button @click="$router.back()">取消</el-button>
-      </el-form-item>
-    </el-form>
-
-    <div class="preview-wrap">
-      <!-- 预览 -->
-      <div class="tip">欢迎语样式</div>
-      <PhoneDialog
-        :message="form.welcomeMsg || '请输入加群引导语'"
-        :isOther="(groupQrCode && groupQrCode.codeUrl) ? true : false"
-      >
-        <el-image class="phone-dialog-image" :src="groupQrCode.codeUrl">
-        </el-image>
-      </PhoneDialog>
-    </div>
-
-    <SelectQrCode
-      :visible.sync="dialogVisibleSelectQrCode"
-      @success="submitSelectQrCode"
-      :selected="codes"
-    ></SelectQrCode>
-  </div>
-</template>
 
 <style lang="scss" scoped>
 .wrap {
