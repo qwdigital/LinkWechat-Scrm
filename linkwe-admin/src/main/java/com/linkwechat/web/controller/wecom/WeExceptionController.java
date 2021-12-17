11f2a1b0cdc4e8564de91ec1f0e392e00c7a5ff2
danmo <775144922@qq.com>
2021-12-17 11:50:09 +0800

    分类bug修改

--- linkwe-admin/src/main/java/com/linkwechat/web/controller/wecom/WeExceptionController.java
+++ linkwe-admin/src/main/java/com/linkwechat/web/controller/wecom/WeExceptionController.java
@@ -3,8 +3,13 @@ package com.linkwechat.web.controller.wecom;
 import com.linkwechat.common.core.domain.AjaxResult;
 import com.linkwechat.common.exception.wecom.WeComException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * @author danmo
@@ -18,6 +23,17 @@ public class WeExceptionController {
     @ExceptionHandler(WeComException.class)
     public AjaxResult weComException(WeComException ex){
         return AjaxResult.error(ex.getCode(),ex.getMessage());
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public AjaxResult customException(MethodArgumentNotValidException e) {
+        // 获取参数校验失败信息
+        List<ObjectError> list = e.getBindingResult().getAllErrors();
+        StringBuilder errorMsg = new StringBuilder();
+        for (ObjectError error : list){
+            errorMsg.append(error.getDefaultMessage()).append("\n");
+        }
+        return AjaxResult.error(errorMsg.toString());
     }
 
     @ExceptionHandler(Exception.class)
--- linkwe-wecom/src/main/java/com/linkwechat/wecom/domain/WeCategory.java
+++ linkwe-wecom/src/main/java/com/linkwechat/wecom/domain/WeCategory.java
@@ -12,6 +12,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @TableName("we_category")
