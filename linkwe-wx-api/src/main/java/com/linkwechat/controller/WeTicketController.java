4dea6e8c675f3b63fa102e6af651c9214fb2cb9b
danmo <775144922@qq.com>
2022-10-24 14:47:59 +0800

    接口调整

--- linkwe-wx-api/src/main/java/com/linkwechat/controller/WeTicketController.java
+++ linkwe-wx-api/src/main/java/com/linkwechat/controller/WeTicketController.java
@@ -8,7 +8,6 @@ import com.linkwechat.domain.wecom.query.WeBaseQuery;
 import com.linkwechat.fegin.QwTicketClient;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,35 +28,14 @@ public class WeTicketController extends BaseController {
     @Resource
     private QwTicketClient qwTicketClient;
 
-
-
-
     /**
-     * 获取企业的jsapi_ticket
-     *
-     * @param url JS接口页面的完整URL
+     * 获取微信应用相关Ticket
      * @return
      */
-    @GetMapping("/getAppTicket")
-    public AjaxResult getAppTicket(String url) {
-        WeBaseQuery query = new WeBaseQuery();
-        query.setCorpid(SecurityUtils.getLoginUser().getCorpId());
-        String ticket = qwTicketClient.getCorpTicket(query).getData();
-        return AjaxResult.success(TicketUtils.getSignatureMap(ticket,url));
+    @GetMapping("/getWxTicket")
+    public AjaxResult getWxTicket(String url){
+        return AjaxResult.success(TicketUtils.getSignatureMap(
+                qwTicketClient.getWxTicket().getData()
+                ,url));
     }
-
-    /**
-     * 获取应用的jsapi_ticket
-     *
-     * @param url JS接口页面的完整URL
-     * @return
-     */
-    @GetMapping("/getAgentTicket")
-    public AjaxResult getAgentTicket(String url) {
-        WeBaseQuery query = new WeBaseQuery();
-        query.setCorpid(SecurityUtils.getCorpId());
-        String ticket = qwTicketClient.getAgentTicket(query).getData();
-        return AjaxResult.success(TicketUtils.getSignatureMap(ticket,url));
-    }
-
 }
