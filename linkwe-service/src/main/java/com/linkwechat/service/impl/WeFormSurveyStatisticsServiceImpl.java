37f4fa4fd45684a07af9fcd299e9c25347758fbb
HaoNanSheng <1539136324@qq.com>
2022-11-30 13:33:13 +0800

    移动端客群数量bug修复

--- linkwe-service/src/main/java/com/linkwechat/service/impl/WeFormSurveyStatisticsServiceImpl.java
+++ linkwe-service/src/main/java/com/linkwechat/service/impl/WeFormSurveyStatisticsServiceImpl.java
@@ -143,10 +143,18 @@ public class WeFormSurveyStatisticsServiceImpl extends ServiceImpl<WeFormSurveyS
         for (String channelName : split) {
             //PV
             String pvKey = StringUtils.format(SiteStatsConstants.PREFIX_KEY_PV, belongId, channelName);
-            pv += (Integer) redisTemplate.opsForValue().get(pvKey);
+            Object o = redisTemplate.opsForValue().get(pvKey);
+            if(Objects.nonNull(o)){
+                pv += (Integer)o;
+            }
+
 
             String ipKey = StringUtils.format(SiteStatsConstants.PREFIX_KEY_IP, belongId, channelName);
-            uv += redisTemplate.opsForSet().size(ipKey);
+            Long size = redisTemplate.opsForSet().size(ipKey);
+            if(null != size){
+                uv += size;
+            }
+
         }
         uv = Long.valueOf(uv.intValue() - split.length);
 
