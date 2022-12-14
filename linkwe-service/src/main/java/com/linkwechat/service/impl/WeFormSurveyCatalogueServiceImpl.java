a02c0773dee49effc4f0d46672320e0693528588
WangYX <664160450@qq.com>
2022-12-14 15:10:00 +0800

    '智能表单提交'

--- linkwe-service/src/main/java/com/linkwechat/service/impl/WeFormSurveyCatalogueServiceImpl.java
+++ linkwe-service/src/main/java/com/linkwechat/service/impl/WeFormSurveyCatalogueServiceImpl.java
@@ -4,16 +4,19 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.date.DateUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.linkwechat.common.constant.Constants;
 import com.linkwechat.common.constant.SiteStatsConstants;
 import com.linkwechat.common.exception.wecom.WeComException;
 import com.linkwechat.common.utils.StringUtils;
+import com.linkwechat.domain.WeFormSurveyAnswer;
 import com.linkwechat.domain.WeFormSurveyCatalogue;
-import com.linkwechat.domain.WeFormSurveyStatistics;
 import com.linkwechat.domain.form.query.WeAddFormSurveyCatalogueQuery;
 import com.linkwechat.domain.form.query.WeFormSurveyCatalogueQuery;
 import com.linkwechat.mapper.WeFormSurveyCatalogueMapper;
+import com.linkwechat.service.IWeFormSurveyAnswerService;
 import com.linkwechat.service.IWeFormSurveyCatalogueService;
 import com.linkwechat.service.IWeFormSurveyStatisticsService;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +27,6 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * 问卷-目录列表(WeFormSurveyCatalogue)
@@ -40,6 +42,8 @@ public class WeFormSurveyCatalogueServiceImpl extends ServiceImpl<WeFormSurveyCa
     private IWeFormSurveyStatisticsService weFormSurveyStatisticsService;
     @Resource
     private WeFormSurveyCatalogueMapper weFormSurveyCatalogueMapper;
+    @Resource
+    private IWeFormSurveyAnswerService weFormSurveyAnswerService;
 
     @Resource
     private RedisTemplate redisTemplate;
@@ -100,22 +104,32 @@ public class WeFormSurveyCatalogueServiceImpl extends ServiceImpl<WeFormSurveyCa
         queryWrapper.eq(Objects.nonNull(query.getSurveyState()), WeFormSurveyCatalogue::getSurveyState, query.getSurveyState());
         queryWrapper.eq(Objects.nonNull(query.getGroupId()), WeFormSurveyCatalogue::getGroupId, query.getGroupId());
         queryWrapper.apply(Objects.nonNull(query.getCreateTime()), "date_format(create_time,'%Y-%m-%d') = " + "'" + DateUtil.formatDate(query.getCreateTime()) + "'");
-        queryWrapper.apply(Objects.nonNull(query.getStartDate()), "date_format(create_time,'%Y-%m-%d') >= " + "'" + DateUtil.formatDate(query.getStartDate()) + "'");
-        queryWrapper.apply(Objects.nonNull(query.getEndDate()), "date_format(create_time,'%Y-%m-%d') <= " + "'" + DateUtil.formatDate(query.getEndDate()) + "'");
+        queryWrapper.between(Objects.nonNull(query.getStartDate()) && Objects.nonNull(query.getEndDate()), WeFormSurveyCatalogue::getCreateTime, DateUtil.formatDate(query.getStartDate()) + " 00:00:00", DateUtil.formatDate(query.getEndDate()) + " 23:59:59");
         queryWrapper.eq(WeFormSurveyCatalogue::getDelFlag, 0);
         queryWrapper.orderByDesc(WeFormSurveyCatalogue::getCreateTime);
         List<WeFormSurveyCatalogue> catalogueList = list(queryWrapper);
         if (CollectionUtil.isNotEmpty(catalogueList)) {
-            List<Long> ids = catalogueList.parallelStream().map(WeFormSurveyCatalogue::getId).collect(Collectors.toList());
-            List<WeFormSurveyStatistics> statistics = weFormSurveyStatisticsService.list(new LambdaQueryWrapper<WeFormSurveyStatistics>()
-                    .in(WeFormSurveyStatistics::getBelongId, ids)
-                    .eq(WeFormSurveyStatistics::getDelFlag, 0));
-            for (WeFormSurveyCatalogue catalogue : catalogueList) {
-                List<WeFormSurveyStatistics> tempStatistic = statistics.parallelStream()
-                        .filter(statistic -> Objects.equals(catalogue.getId(), statistic.getBelongId())).collect(Collectors.toList());
-                if (CollectionUtil.isNotEmpty(tempStatistic)) {
-                    catalogue.setTotalVisits(tempStatistic.get(0).getTotalVisits());
-                    catalogue.setCollectionVolume(tempStatistic.get(0).getCollectionVolume());
+            for (WeFormSurveyCatalogue weFormSurveyCatalogue : catalogueList) {
+                String channelsName = weFormSurveyCatalogue.getChannelsName();
+                Integer pv = 0;
+                String[] split = channelsName.split(",");
+                for (String channelName : split) {
+                    //PV
+                    String pvKey = StringUtils.format(SiteStatsConstants.PREFIX_KEY_PV, weFormSurveyCatalogue.getId(), channelName);
+                    pv += (Integer) redisTemplate.opsForValue().get(pvKey);
+                }
+                //总访问数
+                weFormSurveyCatalogue.setTotalVisits(pv);
+                //有效的收集量
+                QueryWrapper<WeFormSurveyAnswer> wrapper = new QueryWrapper<>();
+                wrapper.lambda().eq(WeFormSurveyAnswer::getBelongId, weFormSurveyCatalogue.getId());
+                wrapper.lambda().eq(WeFormSurveyAnswer::getAnEffective, 0);
+                wrapper.lambda().eq(WeFormSurveyAnswer::getDelFlag, Constants.NORMAL_CODE);
+                List<WeFormSurveyAnswer> list = weFormSurveyAnswerService.list(wrapper);
+                if (list != null) {
+                    weFormSurveyCatalogue.setCollectionVolume(list.size());
+                } else {
+                    weFormSurveyCatalogue.setCollectionVolume(0);
                 }
             }
         }
