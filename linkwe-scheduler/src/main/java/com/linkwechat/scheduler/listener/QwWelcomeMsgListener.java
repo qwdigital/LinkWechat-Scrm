ced84dd01b4532631834ad8116177cc0f2227f3f
WangYX <664160450@qq.com>
2023-02-06 10:57:40 +0800

    员工欢迎语bug修复

--- linkwe-scheduler/src/main/java/com/linkwechat/scheduler/listener/QwWelcomeMsgListener.java
+++ linkwe-scheduler/src/main/java/com/linkwechat/scheduler/listener/QwWelcomeMsgListener.java
@@ -4,6 +4,7 @@ import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.collection.ListUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.linkwechat.common.config.LinkWeChatConfig;
@@ -11,12 +12,15 @@ import com.linkwechat.common.constant.Constants;
 import com.linkwechat.common.constant.WeConstans;
 import com.linkwechat.common.context.SecurityContextHolder;
 import com.linkwechat.common.enums.MessageType;
+import com.linkwechat.common.enums.WeMsgTypeEnum;
 import com.linkwechat.common.utils.StringUtils;
 import com.linkwechat.common.utils.spring.SpringUtils;
 import com.linkwechat.domain.*;
 import com.linkwechat.domain.community.vo.WeCommunityWeComeMsgVo;
 import com.linkwechat.domain.customer.WeMakeCustomerTag;
+import com.linkwechat.domain.material.entity.WeMaterial;
 import com.linkwechat.domain.media.WeMessageTemplate;
+import com.linkwechat.domain.msgtlp.entity.WeTlpMaterial;
 import com.linkwechat.domain.msgtlp.query.WeMsgTlpQuery;
 import com.linkwechat.domain.msgtlp.vo.WeMsgTlpVo;
 import com.linkwechat.domain.qr.WeQrAttachments;
@@ -30,6 +34,7 @@ import com.linkwechat.domain.wecom.query.customer.msg.WeWelcomeMsgQuery;
 import com.linkwechat.domain.wecom.vo.WeResultVo;
 import com.linkwechat.domain.wecom.vo.media.WeMediaVo;
 import com.linkwechat.fegin.QwCustomerClient;
+import com.linkwechat.mapper.WeTlpMaterialMapper;
 import com.linkwechat.service.*;
 import com.linkwechat.service.impl.WeCorpAccountServiceImpl;
 import com.rabbitmq.client.Channel;
@@ -41,6 +46,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -95,6 +101,9 @@ public class QwWelcomeMsgListener {
 
     @Value("${wecom.welcome-msg-default}")
     private String welcomeMsgDefault;
+
+    @Resource
+    private WeTlpMaterialMapper weTlpMaterialMapper;
 
 
     @RabbitHandler
@@ -208,11 +217,39 @@ public class QwWelcomeMsgListener {
                 WeMsgTlpQuery weMsgTlpQuery = new WeMsgTlpQuery();
                 weMsgTlpQuery.setUserId(query.getUserID());
                 weMsgTlpQuery.setFlag(false);
+                //员工欢迎语类型
+                weMsgTlpQuery.setTplType(2);
                 List<WeMsgTlpVo> weMsgTlpList = weMsgTlpService.getList(weMsgTlpQuery);
                 if (CollectionUtil.isNotEmpty(weMsgTlpList)) {
-                    WeMsgTlpVo weMsgTlpVo = weMsgTlpList.stream().findFirst().get();
-                    List<WeMessageTemplate> attachments = weMsgTlpVo.getAttachments();
-                    templates.addAll(attachments);
+                    Optional<WeMsgTlpVo> first = weMsgTlpList.stream().findFirst();
+                    first.ifPresent(o -> {
+                        WeMsgTlpVo weMsgTlpVo = first.get();
+                        //文本内容
+                        WeMessageTemplate weMessageTemplate = new WeMessageTemplate();
+                        weMessageTemplate.setMsgType(WeMsgTypeEnum.TEXT.getMessageType());
+                        weMessageTemplate.setContent(weMsgTlpVo.getTemplateInfo());
+                        templates.add(weMessageTemplate);
+                        //附件
+                        LambdaQueryWrapper<WeTlpMaterial> queryWrapper = new LambdaQueryWrapper<>();
+                        queryWrapper.eq(WeTlpMaterial::getTlpId, weMsgTlpVo.getId());
+                        List<WeTlpMaterial> weTlpMaterials = weTlpMaterialMapper.selectList(queryWrapper);
+                        List<Long> collect = weTlpMaterials.stream().map(x -> x.getMaterialId()).collect(Collectors.toList());
+                        if (collect != null && collect.size() > 0) {
+                            LambdaQueryWrapper<WeMaterial> wrapper = new LambdaQueryWrapper<>();
+                            wrapper.in(WeMaterial::getId, collect);
+                            List<WeMaterial> weMaterials = weMaterialService.list(wrapper);
+                            if (weMaterials != null && weMaterials.size() > 0) {
+                                for (WeMaterial weMaterial : weMaterials) {
+                                    templates.add(materialToWeMessageTemplate(weMaterial));
+                                }
+                            }
+                        }
+                    });
+
+
+//                    WeMsgTlpVo weMsgTlpVo = weMsgTlpList.stream().findFirst().get();
+//                    List<WeMessageTemplate> attachments = weMsgTlpVo.getAttachments();
+//                    templates.addAll(attachments);
                 }
             }
             WeResultVo resultDto = sendWelcomeMsg(query, templates);
@@ -284,5 +321,75 @@ public class QwWelcomeMsgListener {
         welcomeMsg.setAttachmentsList(linkWeChatConfig.getH5Domain(), attachments);
         return qwCustomerClient.sendWelcomeMsg(welcomeMsg).getData();
     }
+
+
+    /**
+     * 素材转消息模板
+     *
+     * @author WangYX
+     * @date 2023/02/03 18:15
+     * @version 1.0.0
+     */
+    private WeMessageTemplate materialToWeMessageTemplate(WeMaterial weMaterial) {
+        WeMessageTemplate weMessageTemplate = new WeMessageTemplate();
+        String mediaType = weMaterial.getMediaType();
+        //
+        switch (mediaType) {
+            //图片
+            //海报
+            case "0":
+            case "5":
+                weMessageTemplate.setMsgType(WeMsgTypeEnum.IMAGE.getMessageType());
+                weMessageTemplate.setPicUrl(weMaterial.getMaterialUrl());
+                break;
+
+            //小程序
+            case "11":
+                weMessageTemplate.setMsgType(WeMsgTypeEnum.MINIPROGRAM.getMessageType());
+                weMessageTemplate.setTitle(weMaterial.getMaterialName());
+                weMessageTemplate.setPicUrl(weMaterial.getCoverUrl());
+                weMessageTemplate.setAppId(weMaterial.getDigest());
+                weMessageTemplate.setLinkUrl(weMaterial.getMaterialUrl());
+                break;
+            //图文
+            case "9":
+                weMessageTemplate.setMsgType(WeMsgTypeEnum.LINK.getMessageType());
+                weMessageTemplate.setPicUrl(weMaterial.getCoverUrl());
+                weMessageTemplate.setTitle(weMaterial.getMaterialName());
+                weMessageTemplate.setDescription(StrUtil.isNotBlank(weMaterial.getContent()) ? weMaterial.getContent() : "");
+                weMessageTemplate.setLinkUrl(weMaterial.getMaterialUrl());
+                break;
+            //文章
+            case "12":
+                weMessageTemplate.setMsgType(WeMsgTypeEnum.NEWS.getMessageType());
+                weMessageTemplate.setPicUrl(weMaterial.getCoverUrl());
+                weMessageTemplate.setTitle(weMaterial.getMaterialName());
+                weMessageTemplate.setDescription(weMaterial.getDigest());
+                weMessageTemplate.setMaterialId(weMaterial.getId());
+                break;
+            //视频
+            case "2":
+                weMessageTemplate.setMsgType(WeMsgTypeEnum.VIDEO.getMessageType());
+                weMessageTemplate.setPicUrl(weMaterial.getCoverUrl());
+                weMessageTemplate.setTitle(weMaterial.getMaterialName());
+                weMessageTemplate.setDescription(weMaterial.getDigest());
+                weMessageTemplate.setMaterialId(weMaterial.getId());
+                weMessageTemplate.setLinkUrl(weMaterial.getMaterialUrl());
+                break;
+            //文件
+            case "3":
+                weMessageTemplate.setMsgType(WeMsgTypeEnum.FILE.getMessageType());
+                weMessageTemplate.setPicUrl(weMaterial.getCoverUrl());
+                weMessageTemplate.setTitle(weMaterial.getMaterialName());
+                weMessageTemplate.setDescription(weMaterial.getDigest());
+                weMessageTemplate.setMaterialId(weMaterial.getId());
+                weMessageTemplate.setLinkUrl(weMaterial.getMaterialUrl());
+                break;
+        }
+
+
+        return weMessageTemplate;
+    }
+
 
 }
