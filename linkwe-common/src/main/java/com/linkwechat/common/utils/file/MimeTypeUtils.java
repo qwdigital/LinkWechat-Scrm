73b21453a0d212c24e1b51679385218f0e6b469b
HaoNanSheng <1539136324@qq.com>
2023-03-21 18:17:12 +0800

    文件服务集成阿里云oss

--- linkwe-common/src/main/java/com/linkwechat/common/utils/file/MimeTypeUtils.java
+++ linkwe-common/src/main/java/com/linkwechat/common/utils/file/MimeTypeUtils.java
@@ -32,7 +32,7 @@ public class MimeTypeUtils
             // 压缩文件
             "rar", "zip", "gz", "bz2",
             // pdf
-            "pdf" ,"wav","amr","mp4","mp3","p12"};
+            "pdf" ,"wav","amr","mp4","mp3","p12","md"};
 
     public static String getExtension(String prefix)
     {
