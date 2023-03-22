1ed13f77b8f1319f3f474a3d21b5c8b453d77231
HaoNanSheng <1539136324@qq.com>
2023-03-22 13:56:19 +0800

    =====

--- linkwe-event-task/src/main/java/com/linkwechat/factory/impl/customer/WeCallBackAddExternalContactImpl.java
+++ linkwe-event-task/src/main/java/com/linkwechat/factory/impl/customer/WeCallBackAddExternalContactImpl.java
@@ -8,7 +8,6 @@ import com.linkwechat.domain.wecom.callback.WeBackBaseVo;
 import com.linkwechat.domain.wecom.callback.WeBackCustomerVo;
 import com.linkwechat.factory.WeEventStrategy;
 import com.linkwechat.service.IWeCustomerService;
-import com.linkwechat.service.IWeTaskFissionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +25,9 @@ public class WeCallBackAddExternalContactImpl extends WeEventStrategy {
     @Autowired
     private IWeCustomerService weCustomerService;
 
-    @Autowired
-    private IWeTaskFissionService weTaskFissionService;
+//    @Autowired
+//    private IWeTaskFissionService weTaskFissionService;
+
 
     @Autowired
     private RabbitMQSettingConfig rabbitMQSettingConfig;
@@ -51,15 +51,15 @@ public class WeCallBackAddExternalContactImpl extends WeEventStrategy {
 
 
         //任务宝裂变客户处理
-        if (StringUtils.isNotEmpty(customerInfo.getState()) && isFission(customerInfo.getState())) {
-            try {
-                String fissionRecordId = customerInfo.getState().substring(WeConstans.FISSION_PREFIX.length());
-                weTaskFissionService.addCustomerHandler(customerInfo.getExternalUserID(),customerInfo.getUserID(),fissionRecordId);
-            } catch (Exception e) {
-                e.printStackTrace();
-                log.error("添加外部联系人异常 任务宝裂变客户处理 params:{}",JSONObject.toJSONString(message),e);
-            }
-        }
+//        if (StringUtils.isNotEmpty(customerInfo.getState()) && isFission(customerInfo.getState())) {
+//            try {
+//                String fissionRecordId = customerInfo.getState().substring(WeConstans.FISSION_PREFIX.length());
+//                weTaskFissionService.addCustomerHandler(customerInfo.getExternalUserID(),customerInfo.getUserID(),fissionRecordId);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                log.error("添加外部联系人异常 任务宝裂变客户处理 params:{}",JSONObject.toJSONString(message),e);
+//            }
+//        }
 
     }
 
--- linkwe-event-task/src/main/java/com/linkwechat/factory/impl/customer/WeCallBackAddHalfExternalContactImpl.java
+++ linkwe-event-task/src/main/java/com/linkwechat/factory/impl/customer/WeCallBackAddHalfExternalContactImpl.java
@@ -1,14 +1,12 @@
 package com.linkwechat.factory.impl.customer;
 
 import com.alibaba.fastjson.JSONObject;
-import com.linkwechat.common.constant.WeConstans;
 import com.linkwechat.common.utils.StringUtils;
 import com.linkwechat.config.rabbitmq.RabbitMQSettingConfig;
 import com.linkwechat.domain.wecom.callback.WeBackBaseVo;
 import com.linkwechat.domain.wecom.callback.WeBackCustomerVo;
 import com.linkwechat.factory.WeEventStrategy;
 import com.linkwechat.service.IWeCustomerService;
-import com.linkwechat.service.IWeTaskFissionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +24,8 @@ public class WeCallBackAddHalfExternalContactImpl extends WeEventStrategy {
     @Autowired
     private IWeCustomerService weCustomerService;
 
-    @Autowired
-    private IWeTaskFissionService weTaskFissionService;
+//    @Autowired
+//    private IWeTaskFissionService weTaskFissionService;
 
     @Autowired
     private RabbitMQSettingConfig rabbitMQSettingConfig;
@@ -53,16 +51,16 @@ public class WeCallBackAddHalfExternalContactImpl extends WeEventStrategy {
             log.error("添加外部联系人异常 params:{}",JSONObject.toJSONString(message),e);
         }
 
-        //任务宝裂变客户处理
-        if (StringUtils.isNotEmpty(customerInfo.getState()) && isFission(customerInfo.getState())) {
-            try {
-                String fissionRecordId = customerInfo.getState().substring(WeConstans.FISSION_PREFIX.length());
-                weTaskFissionService.addCustomerHandler(customerInfo.getExternalUserID(), customerInfo.getUserID(), fissionRecordId);
-            } catch (Exception e) {
-                e.printStackTrace();
-                log.error("添加外部联系人异常 任务宝裂变客户处理 params:{}",JSONObject.toJSONString(message),e);
-            }
-        }
+//        //任务宝裂变客户处理
+//        if (StringUtils.isNotEmpty(customerInfo.getState()) && isFission(customerInfo.getState())) {
+//            try {
+//                String fissionRecordId = customerInfo.getState().substring(WeConstans.FISSION_PREFIX.length());
+//                weTaskFissionService.addCustomerHandler(customerInfo.getExternalUserID(), customerInfo.getUserID(), fissionRecordId);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                log.error("添加外部联系人异常 任务宝裂变客户处理 params:{}",JSONObject.toJSONString(message),e);
+//            }
+//        }
 
     }
 
