58523f69191f68471ec2f820e427659dfbd818dd
HaoNanSheng <1539136324@qq.com>
2023-03-22 11:39:52 +0800

    裂变api提交

--- linkwe-wx-api/src/main/java/com/linkwechat/controller/WxTaskFissionController.java
+++ linkwe-wx-api/src/main/java/com/linkwechat/controller/WxTaskFissionController.java
@@ -12,12 +12,11 @@ import org.springframework.web.bind.annotation.*;
 
 
 /**
- * 任务宝Controller
+ * 任务裂变(微信)
  *
  * @author leejoker
  * @date 2021-01-20
  */
-@Api(tags = "任务宝Controller")
 @RestController
 @RequestMapping("/fission")
 public class WxTaskFissionController extends BaseController {
@@ -34,8 +33,8 @@ public class WxTaskFissionController extends BaseController {
 
     /**
      * 获取生成指定客户的裂变海报
-     * @param unionid
-     * @param fissionId
+     * @param unionid 微信客户unionid
+     * @param fissionId 裂变id
      * @return
      */
     @GetMapping("/findFissionPoster")
@@ -51,8 +50,8 @@ public class WxTaskFissionController extends BaseController {
 
     /**
      * 获取裂变进度
-     * @param unionid
-     * @param fissionId
+     * @param unionid 微信客户unionid
+     * @param fissionId 裂变id
      * @return
      */
     @GetMapping("/findWeFissionProgress")
