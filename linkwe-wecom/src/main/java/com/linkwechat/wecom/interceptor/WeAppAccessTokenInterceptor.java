cc5c6d108fd8f262676ac94a37f091b302128f8d
danmo <775144922@qq.com>
2023-03-28 17:11:20 +0800

    日志级别修改

--- linkwe-wecom/src/main/java/com/linkwechat/wecom/interceptor/WeAppAccessTokenInterceptor.java
+++ linkwe-wecom/src/main/java/com/linkwechat/wecom/interceptor/WeAppAccessTokenInterceptor.java
@@ -55,7 +55,7 @@ public class WeAppAccessTokenInterceptor extends WeForestInterceptor implements 
      */
     @Override
     public void onError(ForestRuntimeException e, ForestRequest forestRequest, ForestResponse forestResponse) {
-        log.info("onError url:{},------params:{},----------result:{}",forestRequest.getUrl(), JSONObject.toJSONString(forestRequest.getArguments()), forestResponse.getContent());
+        log.error("onError url:{},------params:{},----------result:{}",forestRequest.getUrl(), JSONObject.toJSONString(forestRequest.getArguments()), forestResponse.getContent());
         if (StringUtils.isNotEmpty(forestResponse.getContent())) {
             WeComException weComException = new WeComException(1001, forestResponse.getContent());
             throw new ForestRuntimeException(weComException);
