fa93e362e6bf0925a0750d321753372a7197c317
WangYX <664160450@qq.com>
2023-08-29 17:26:25 +0800

    客户数据导出-商机阶段问题处理

--- linkwe-service/src/main/java/com/linkwechat/converter/CustomerTrackStateConverter.java
+++ linkwe-service/src/main/java/com/linkwechat/converter/CustomerTrackStateConverter.java
@@ -1,5 +1,6 @@
 package com.linkwechat.converter;
 
+import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.util.StrUtil;
 import com.alibaba.excel.converters.Converter;
 import com.alibaba.excel.converters.WriteConverterContext;
@@ -12,6 +13,7 @@ import com.linkwechat.domain.WeStrackStage;
 import com.linkwechat.service.IWeStrackStageService;
 import org.apache.commons.collections.map.HashedMap;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -25,14 +27,16 @@ import java.util.stream.Collectors;
  */
 public class CustomerTrackStateConverter implements Converter<Integer> {
 
-    private final Map<Integer, String> map;
+    private Map<Integer, String> map = new HashMap<>();
 
     public CustomerTrackStateConverter() {
         IWeStrackStageService service = SpringUtils.getBean(IWeStrackStageService.class);
         LambdaQueryWrapper<WeStrackStage> queryWrapper = Wrappers.lambdaQuery(WeStrackStage.class);
         queryWrapper.eq(WeStrackStage::getDelFlag, Constants.COMMON_STATE);
         List<WeStrackStage> list = service.list(queryWrapper);
-        map = list.stream().collect(Collectors.toMap(WeStrackStage::getStageVal, WeStrackStage::getStageKey));
+        if (CollectionUtil.isNotEmpty(list)) {
+            map = list.stream().collect(Collectors.toMap(WeStrackStage::getStageVal, WeStrackStage::getStageKey));
+        }
     }
 
     @Override
