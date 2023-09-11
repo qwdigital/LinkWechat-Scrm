652b944983ef12374a0fce32938c8da8d7d3532c
HaoNanSheng <1539136324@qq.com>
2023-09-11 16:37:34 +0800

    朋友圈列表增加权限过滤

--- linkwe-service/src/main/java/com/linkwechat/mapper/WeMomentsTaskMapper.java
+++ linkwe-service/src/main/java/com/linkwechat/mapper/WeMomentsTaskMapper.java
@@ -1,6 +1,8 @@
 package com.linkwechat.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.linkwechat.common.annotation.DataColumn;
+import com.linkwechat.common.annotation.DataScope;
 import com.linkwechat.domain.moments.entity.WeMomentsTask;
 import com.linkwechat.domain.moments.query.WeMomentsTaskListRequest;
 import com.linkwechat.domain.moments.vo.WeMomentsTaskVO;
@@ -24,6 +26,7 @@ public interface WeMomentsTaskMapper extends BaseMapper<WeMomentsTask> {
      * @author WangYX
      * @date 2023/06/06 18:10
      */
+    @DataScope(type = "2", value = @DataColumn(alias = "t1", name = "create_by_id", userid = "user_id"))
     List<WeMomentsTaskVO> list(WeMomentsTaskListRequest request);
 
 }
