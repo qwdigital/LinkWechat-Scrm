79300ac20df144026af2b43e9da42d4d80aae456
HaoNanSheng <1539136324@qq.com>
2023-11-23 15:32:28 +0800

    =====

--- linkwe-scheduler/src/main/java/com/linkwechat/scheduler/listener/QwAppMsgListener.java
+++ linkwe-scheduler/src/main/java/com/linkwechat/scheduler/listener/QwAppMsgListener.java
@@ -11,8 +11,6 @@ import com.linkwechat.domain.WeShortLinkPromotionTemplateAppMsg;
 import com.linkwechat.domain.msg.QwAppMsgBody;
 import com.linkwechat.domain.shortlink.dto.WeShortLinkPromotionAppMsgDto;
 import com.linkwechat.scheduler.service.AbstractAppMsgService;
-import com.linkwechat.service.IWeShortLinkPromotionService;
-import com.linkwechat.service.IWeShortLinkPromotionTemplateAppMsgService;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
@@ -31,11 +29,6 @@ import java.util.Objects;
 @Slf4j
 @Component
 public class QwAppMsgListener {
-
-    @Resource
-    private IWeShortLinkPromotionService weShortLinkPromotionService;
-    @Resource
-    private IWeShortLinkPromotionTemplateAppMsgService weShortLinkPromotionTemplateAppMsgService;
 
 
     @RabbitHandler
@@ -65,68 +58,6 @@ public class QwAppMsgListener {
         }
     }
 
-
-    /**
-     * 短链推广 - 企微延迟应用消息监听
-     *
-     * @param msg
-     * @param channel
-     * @param message
-     */
-    @RabbitHandler
-    @RabbitListener(queues = "${wecom.mq.queue.delay_app-msg:Qu_Delay_AppMsg}")
-    public void delaySubscribe(String msg, Channel channel, Message message) {
-        try {
-            log.info("企微延迟应用消息监听：msg:{}", msg);
-            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
-
-            WeShortLinkPromotionAppMsgDto appMsgBody = JSONObject.parseObject(msg, WeShortLinkPromotionAppMsgDto.class);
-
-            //1.判断任务是否处于待推广的状态
-            WeShortLinkPromotion weShortLinkPromotion = weShortLinkPromotionService.getById(appMsgBody.getShortLinkPromotionId());
-
-            if (BeanUtil.isEmpty(weShortLinkPromotion)) {
-                return;
-            } else {
-                //判断任务的推广状态和删除状态
-                //删除标识 0 正常 1 删除
-                //任务状态: 0待推广 1推广中 2已结束
-                if (!(weShortLinkPromotion.getDelFlag().equals(0) && weShortLinkPromotion.getTaskStatus().equals(0))) {
-                    return;
-                }
-            }
-
-            //查看短链推广模板是否已经删除
-            Long businessId = appMsgBody.getBusinessId();
-            if (weShortLinkPromotion.getType().equals(3)) {
-                WeShortLinkPromotionTemplateAppMsg appMsg = weShortLinkPromotionTemplateAppMsgService.getById(businessId);
-                if (appMsg == null || appMsg.getDelFlag().equals(1)) {
-                    //删除直接跳出，不继续执行。
-                    return;
-                }
-            }
-
-            QwAppMsgBusinessTypeEnum qwAppMsgBusinessTypeEnum = QwAppMsgBusinessTypeEnum.parseEnum(appMsgBody.getBusinessType());
-            if (Objects.nonNull(qwAppMsgBusinessTypeEnum)) {
-                switch (qwAppMsgBusinessTypeEnum) {
-                    case AGENT:
-                        SpringUtils.getBean(qwAppMsgBusinessTypeEnum.getBeanName(), AbstractAppMsgService.class).sendAgentMsg(appMsgBody);
-                        break;
-                    default: //默认通用发送
-                        SpringUtils.getBean(qwAppMsgBusinessTypeEnum.getBeanName(), AbstractAppMsgService.class).sendAppMsg(appMsgBody);
-                        //修改状态为推广中
-                        LambdaUpdateWrapper<WeShortLinkPromotion> promotionUpdate = Wrappers.lambdaUpdate();
-                        promotionUpdate.set(WeShortLinkPromotion::getTaskStatus, 1);
-                        promotionUpdate.eq(WeShortLinkPromotion::getId, weShortLinkPromotion.getId());
-                        weShortLinkPromotionService.update(promotionUpdate);
-                        break;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            log.error("企微延迟应用消息监听-消息处理失败 msg:{},error:{}", msg, e);
-        }
-    }
 
 
 }
