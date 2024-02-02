74a62d0b61e1dc3f17bc8dd96370d5b051cfe4b2
HaoNanSheng <1539136324@qq.com>
2024-02-02 14:13:48 +0800

    客服密钥设置为客户密钥

--- linkwe-api/src/main/java/com/linkwechat/controller/WeCorpAccountController.java
+++ linkwe-api/src/main/java/com/linkwechat/controller/WeCorpAccountController.java
@@ -1,22 +1,16 @@
 package com.linkwechat.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.linkwechat.common.config.LinkWeChatConfig;
 import com.linkwechat.common.core.controller.BaseController;
 import com.linkwechat.common.core.domain.AjaxResult;
-import com.linkwechat.common.utils.StringUtils;
 import com.linkwechat.domain.WeCorpAccount;
 import com.linkwechat.domain.media.WeMessageTemplate;
 import com.linkwechat.domain.welcomemsg.WeDefaultWelcomeMsg;
-import com.linkwechat.domain.welcomemsg.vo.WeDefaultWelcomeMsgVo;
-import com.linkwechat.fegin.QwCorpClient;
 import com.linkwechat.service.IWeCorpAccountService;
 import com.linkwechat.service.IWeDefaultWelcomeMsgService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
 import java.util.List;
 
 
@@ -34,8 +28,6 @@ public class WeCorpAccountController extends BaseController {
     private IWeDefaultWelcomeMsgService iWeDefaultWelcomeMsgService;
 
 
-    @Autowired
-    private  QwCorpClient qwCorpClient;
 
     /**
      * 获取当前租户信息
@@ -60,7 +52,7 @@ public class WeCorpAccountController extends BaseController {
         if (linkWeChatConfig.isDemoEnviron()) {
             return AjaxResult.error("当前为演示环境,无法修改配置");
         }
-
+        weCorpAccount.setKfSecret(weCorpAccount.getAgentSecret());
         iWeCorpAccountService.addOrUpdate(weCorpAccount);
         return AjaxResult.success();
     }
@@ -89,46 +81,6 @@ public class WeCorpAccountController extends BaseController {
         return AjaxResult.success("操作成功", iWeCorpAccountService.getCustomerChurnNoticeSwitch());
     }
 
-//    /**
-//     * 商城配置
-//     *
-//     * @param weCorpAccount
-//     */
-//    private void saveOrUpdateShopConfig(WeCorpAccount weCorpAccount) {
-//        //商城系统新增或修改
-//        try {
-//            JSONObject jsonObject = new JSONObject();
-//            //小程序配置
-//            if (StringUtils.isNotBlank(weCorpAccount.getShopAppId())) {
-//                jsonObject.put("wxapp_appId", weCorpAccount.getShopAppId().trim());
-//            }
-//            if (StringUtils.isNotBlank(weCorpAccount.getShopSecret())) {
-//                jsonObject.put("wxapp_secret", weCorpAccount.getShopSecret().trim());
-//            }
-//            //支付配置
-//            if (StringUtils.isNotBlank(weCorpAccount.getMerChantNumber())) {
-//                jsonObject.put("wxpay_mchId", weCorpAccount.getMerChantNumber().trim());
-//            }
-//            if (StringUtils.isNotBlank(weCorpAccount.getMerChantSecret())) {
-//                jsonObject.put("wxpay_mchKey", weCorpAccount.getMerChantSecret().trim());
-//            }
-//            if (StringUtils.isNotBlank(weCorpAccount.getCertP12Url())) {
-//                jsonObject.put("wxpay_keyPath", weCorpAccount.getCertP12Url().trim());
-//            }
-//            //小程序消息消息推送配置
-//            if (StringUtils.isNotBlank(weCorpAccount.getShopMaToken())) {
-//                jsonObject.put("wechat_ma_token", weCorpAccount.getShopMaToken().trim());
-//            }
-//            if (StringUtils.isNotBlank(weCorpAccount.getShopMaEncodingaeskey())) {
-//                jsonObject.put("wechat_ma_encodingaeskey", weCorpAccount.getShopMaEncodingaeskey().trim());
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            logger.error("商城系统-小程序配置失败！！！");
-//        }
-//    }
-//
 
     /**
      * 设置默认欢迎语
